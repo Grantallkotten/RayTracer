96379bfece81baa686907789b791d6e67126ba53
Wilhelm Björkström <143391787+Grantallkotten@users.noreply.github.com>
2023-09-15 14:17:57 +0200

    Clean up

--- src/ray.cpp
+++ src/ray.cpp
@@ -5,16 +5,11 @@
 
 
 ColorDBL Ray::castRay(Scene* theScene, Ray* prevRay, float deathProbability) {
-	//@TODO kolla igenom denna för optimering och förbättring
-	// Loop for collision
-	// If no collision return and set color to skybox
-	// Calc nev ray
-	// Send new ray
+	// Är prevRay nödvändig?
 
 	float minDist = std::numeric_limits<float>::max();
 	float newDist = std::numeric_limits<float>::max();
 	bool hitsObject = false;
-	bool oneHit = false;
 
 	glm::vec3 intersectionPoint = glm::vec3();
 	glm::vec3 newIntersectionPoint = glm::vec3();
@@ -44,22 +39,8 @@ ColorDBL Ray::castRay(Scene* theScene, Ray* prevRay, float deathProbability) {
 		return obj->getMaterial().getColor(); //@TODO * imortance sen och räkna med speculäritet
 	}
 
+	//@TODO Rekursiv formel
 
-	// Calc new dir
-
-	/*
-	// Test collision
-	// Creat a new outgoing vec
-	glm::vec3 newDir = glm::vec3(1.0f, 0.0f, 0.0f);
-	glm::vec3 newOrig = glm::vec3(1.0f, 0.0f, 0.0f);
-
-	Ray nextRay = Ray(newOrig, newDir);
-
-	if (false) {// lastRay == false
-		//next = *nextRay->castRay(theScene, this, true);
-	}
-	
- */
 	return ColorDBL(0.0f, 0.0f, 0.2f);
 }
 
