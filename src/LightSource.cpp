58c98bb68599a3b3aead971ad5558a069fc7d89a
Emil Wallberg <ew2001@live.se>
2023-09-22 13:14:47 +0200

    Fixed all the warnings

--- src/LightSource.cpp
+++ src/LightSource.cpp
@@ -1,49 +1,49 @@
-#pragma once
 #include "../include/LightSource.h"
 #include "../include/Scene.h"
 #include <random>
-# define _PI 3.14159265358979323846
+#define _PI 3.14159265358979323846
 
 std::default_random_engine seed;
 std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
 
-double LightSource::CheckShadowRays(Scene* scene, Object* objectX, const glm::vec3& x) {
+double LightSource::CheckShadowRays(Scene *scene, Object *objectX,
+                                    const glm::vec3 &x) {
 
-	float sum = 0.0;
-	int N = 50;
-	float A = glm::length(E1) * glm::length(E2) / 2; // Kolla så denna är tänkt rätt med punkter
-	glm::vec3 Ny = getNormal();
-	glm::vec3 Nx = objectX->getNormal();
+  float sum = 0.0;
+  int N = 50;
+  float A = glm::length(E1) * glm::length(E2) /
+            2; // Kolla så denna är tänkt rätt med punkter
+  glm::vec3 Ny = getNormal();
+  glm::vec3 Nx = objectX->getNormal();
 
-	if (typeid(*objectX) == typeid(LightSource)) {
-		return 1.0;
-	}
+  if (typeid(*objectX) == typeid(LightSource)) {
+    return 1.0;
+  }
 
-	for (int i = 1; i <= N; i++) {
-		float s = ((float)rand() / (RAND_MAX));
-		float t = ((float)rand() / (RAND_MAX));
+  for (int i = 1; i <= N; i++) {
+    float s = ((float)rand() / (RAND_MAX));
+    float t = ((float)rand() / (RAND_MAX));
 
-		if (s + t > 1) {
-			s = 1 - s;
-			t = 1 - t;
-		}
+    if (s + t > 1) {
+      s = 1 - s;
+      t = 1 - t;
+    }
 
-		glm::vec3 yi = getP0() + s * E1 + t* E2;
-		glm::vec3 di = yi - x;
-		//std::cout << "\n\n\n";
-		//std::cout << "d_i: " << di.x << " " << di.y << " " << di.z << "\n\n\n";
-		// @TODO Check if no collission V(x,y_i) and do it right
-		if (!Ray(x, di).ShadowRay(scene)) { continue; }
+    glm::vec3 yi = getP0() + s * E1 + t * E2;
+    glm::vec3 di = yi - x;
+    // std::cout << "\n\n\n";
+    // std::cout << "d_i: " << di.x << " " << di.y << " " << di.z << "\n\n\n";
+    //  @TODO Check if no collission V(x,y_i) and do it right
+    if (!Ray(x, di).ShadowRay(scene)) {
+      continue;
+    }
 
-		float cosX = glm::dot(Ny, di/glm::length(di));
-		float cosY = -glm::dot(Nx, di / glm::length(di));
+    float cosX = glm::dot(Ny, di / glm::length(di));
+    float cosY = -glm::dot(Nx, di / glm::length(di));
 
-		sum += std::max(0.0f, (cosX * cosY) / (glm::length(di) * glm::length(di)));
+    sum += std::max(0.0f, (cosX * cosY) / (glm::length(di) * glm::length(di)));
+  }
+  float BRDF = 1.0f / _PI;
 
-	}
-	float BRDF = 1.0f / _PI;
-
-	return (float)(A * BRDF * radiance / N * sum);
+  return (float)(A * BRDF * radiance / N * sum);
 }
-
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,13 @@
-// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
-#pragma once
-#include <iostream>
+// main.cpp : This file contains the 'main' function. Program execution begins
+// and ends there.
 #include "../include/Scene.h"
-
+#include <iostream>
 
 // https://www.youtube.com/watch?v=gfW1Fhd9u9Q&list=PLlrATfBNZ98edc5GshdBtREv5asFW3yXl
 // https://raytracing.github.io/books/RayTracingInOneWeekend.html
 // https://github.com/3DJakob/tncg15-ray-tracer/blob/main/TNCG15%20Ray%20Tracer/Classes/camera.h
 
-int main()
-{
-    std::cout << "Hello World!\n";
-
-    Scene();
-    
-
-    //c.writePPM();
+int main() {
+  std::cout << "Hello World!\n";
+  Scene();
 }
-
